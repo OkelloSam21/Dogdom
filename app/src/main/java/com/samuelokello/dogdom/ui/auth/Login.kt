9e77e3108c4edfe9f77afda521c7a74f20e1ccb1
OkelloSam <samuelokello929@gmail.com>
2024-08-29 21:18:43 +0300

    Implement login screen UI
    This commit implements the UI for the login screen, including:
    
    - A background image with a semi-transparent black overlay.
    
    - The Dogdom logo.
    - A phone number input field with country code selection.
    - A "Get Captcha" button.
    - A "Password To Login" text button.
    - User agreement and privacy terms text at the bottom.

--- app/src/main/java/com/samuelokello/dogdom/ui/auth/Login.kt
+++ app/src/main/java/com/samuelokello/dogdom/ui/auth/Login.kt
@@ -1,17 +1,201 @@
 package com.samuelokello.dogdom.ui.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.samuelokello.dogdom.R
+import com.samuelokello.dogdom.ui.shared.components.CustomButton
 
 @Composable
 fun LoginScreen() {
 
+    var selectedCountryCode by rememberSaveable { mutableStateOf("+1") }
+    var phoneNumber by rememberSaveable { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopEnd
+    ) {
+        Image(
+            painter = painterResource(R.drawable.login_background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black.copy(alpha = 0.65f))
+        )
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 20.dp)
+        ){
+            Column {
+                Image(
+                    painter = painterResource(R.drawable.dogdom_title),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.size(200.dp)
+                )
+            }
+            Column {
+
+                PhoneWithCountryCodeTextField(
+                    countryCodes = listOf("+1", "+44", "+91", "+254", "+256"), // Add more country codes as needed
+                    selectedCountryCode = selectedCountryCode,
+                    onCountryCodeChange = { selectedCountryCode = it },
+                    phoneNumber = phoneNumber,
+                    onPhoneNumberChange = { phoneNumber = it },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                CustomButton(
+                    text = "Get Captcha",
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                TextButton( onClick = {}) {
+                    Text(
+                        text = "Password To Login",
+                        style = MaterialTheme.typography.labelMedium.copy(color = Color.White)
+                    )
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(horizontal = 16.dp, vertical = 16.dp),
+            contentAlignment = Alignment.Center
+        ){
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                Text(
+                    text = "By signing in, you agree to the User Agreement ",
+                    style = MaterialTheme.typography.labelMedium.copy(color = Color.White),
+                )
+                Text(
+                    text = "and Privacy Terms",
+                    style = MaterialTheme.typography.labelMedium.copy(color = Color.White),
+                )
+            }
+        }
+
+    }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PhoneNumberTextInput(
+fun PhoneWithCountryCodeTextField(
+    countryCodes: List<String>,
+    selectedCountryCode: String,
+    onCountryCodeChange: (String) -> Unit,
     phoneNumber: String,
-    onPhoneNumberChange: (String) -> Unit
+    onPhoneNumberChange: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Row(
+        modifier = modifier
+            .border(1.dp, MaterialTheme.colorScheme.outline, RoundedCornerShape(20.dp))
+    ) {
+        // Country Code TextField
+        Box(modifier = Modifier.weight(0.3f)) {
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded }
+            ) {
+                OutlinedTextField(
+                    value = selectedCountryCode,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                    },
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(
+                        unfocusedBorderColor = Color.Transparent,
+                        focusedBorderColor = Color.Transparent
+                    ),
+                    modifier = Modifier
+                        .menuAnchor()
+                        .fillMaxWidth()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    countryCodes.forEach { code ->
+                        DropdownMenuItem(
+                            text = { Text(text = code) },
+                            onClick = {
+                                onCountryCodeChange(code)
+                                expanded = false
+                            }
+                        )
+                    }
+                }
+            }
+        }
+
+        // Phone Number TextField
+        OutlinedTextField(
+            value = phoneNumber,
+            onValueChange = onPhoneNumberChange,
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(
+                unfocusedBorderColor = Color.Transparent,
+                focusedBorderColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .weight(0.7f)
+                .height(56.dp)
+        )
+    }
+}
 
 
+@Preview(showBackground = true)
+@Composable
+private fun LoginPreview() {
+    LoginScreen()
 }
