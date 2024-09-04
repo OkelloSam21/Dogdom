41f2df4832a230eb5087fe23740198b0fec2ada8
OkelloSam <samuelokello929@gmail.com>
2024-09-04 13:52:48 +0300

    feat: Implement basic navigation
    This commit implements basic navigation between the login screen and the home screen.
    
    - Removes the splash screen animation.
    - Sets up
     a HomeViewModel and injects it into the DogdomScreen composable.
    - Uses the DogdomScreen composable to handle navigation between screens.

--- app/src/main/java/com/samuelokello/dogdom/MainActivity.kt
+++ app/src/main/java/com/samuelokello/dogdom/MainActivity.kt
@@ -1,61 +1,61 @@
 package com.samuelokello.dogdom
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View
-import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.Modifier
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.samuelokello.dogdom.ui.auth.LoginScreen
+import androidx.lifecycle.ViewModelProvider
+import com.samuelokello.dogdom.data.DataSource
+import com.samuelokello.dogdom.navigation.DogdomScreen
+import com.samuelokello.dogdom.ui.home.HomeViewModel
+import com.samuelokello.dogdom.ui.home.HomeViewModelFactory
 import com.samuelokello.dogdom.ui.theme.DogdomTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-        installSplashScreen().apply {
-            setOnExitAnimationListener { screen ->
-                val zoomX = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_X,
-                    0f
-                )
-                zoomX.interpolator = OvershootInterpolator()
-                zoomX.duration = 500L
-                zoomX.doOnEnd { screen.remove() }
-
-
-                val zoomY = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_Y,
-                    0f
-                )
-                zoomY.interpolator = OvershootInterpolator()
-                zoomY.duration = 500L
-                zoomY.doOnEnd { screen.remove() }
-
-                zoomX.start()
-                zoomY.start()
-
-            }
-        }
+        enableEdgeToEdge()
+//        installSplashScreen().apply {
+//            setOnExitAnimationListener { screen ->
+//                val zoomX = ObjectAnimator.ofFloat(
+//                    screen.iconView,
+//                    View.SCALE_X,
+//                    0f
+//                )
+//                zoomX.interpolator = OvershootInterpolator()
+//                zoomX.duration = 300L
+//                zoomX.doOnEnd { screen.remove() }
+//
+//
+//                val zoomY = ObjectAnimator.ofFloat(
+//                    screen.iconView,
+//                    View.SCALE_Y,
+//                    0f
+//                )
+//                zoomY.interpolator = OvershootInterpolator()
+//                zoomY.duration = 300L
+//                zoomY.doOnEnd { screen.remove() }
+//
+//                zoomX.start()
+//                zoomY.start()
+//
+//            }
+//        }
         setContent {
             // A surface container using the 'background' color from the theme
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-                color = MaterialTheme.colorScheme.background
-            ) {
-                DogdomTheme {
-                    LoginScreen()
-                }
+            homeViewModel =
+                ViewModelProvider(this, HomeViewModelFactory(DataSource))[HomeViewModel::class.java]
+            DogdomTheme {
+                DogdomScreen(
+                    homeViewModel = homeViewModel,
+                    modifier = Modifier
+
+                )
             }
         }
     }
 }
-
