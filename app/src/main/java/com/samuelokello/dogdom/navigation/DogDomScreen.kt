3219b3f909cfeb951e7ad38a6522ae39ce23dd05
OkelloSam <samuelokello929@gmail.com>
2024-09-08 11:42:43 +0300

    Implement bottom navigation bar and new screens
    This commit introduces a bottom navigation bar for easier navigation and adds new screens to the app:
    
    - **BottomNavigationBar:** Provides quick access to Home, Circle, Release, Message, and Profile screens.
    - **CircleScreen:**  Displays content related to user circles or communities.
    - **ReleaseScreen:** Shows releases or announcements.
    - **MessageScreen:** Enables users to view and send messages.
    - **ProfileScreen
    :** Allows users to manage their profiles.
    - **SearchScreen:** Facilitates content searching.
    - **NotificationScreen:** Displays user notifications.
    - **CreatePostScreen:** Enables users to create new posts.
    
    The bottom navigation bar is displayed conditionally based on the current screen.

--- app/src/main/java/com/samuelokello/dogdom/navigation/DogDomScreen.kt
+++ app/src/main/java/com/samuelokello/dogdom/navigation/DogDomScreen.kt
@@ -4,20 +4,38 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.samuelokello.dogdom.navigation.bottom_navigation.BottomNavigationBar
+import com.samuelokello.dogdom.ui.NotificationScreen
+import com.samuelokello.dogdom.ui.ProfileScreen
+import com.samuelokello.dogdom.ui.release.ReleaseScreen
 import com.samuelokello.dogdom.ui.auth.LoginScreen
+import com.samuelokello.dogdom.ui.circle.CircleScreen
 import com.samuelokello.dogdom.ui.home.HomeScreen
 import com.samuelokello.dogdom.ui.home.HomeViewModel
+import com.samuelokello.dogdom.ui.message.MessageScreen
+import com.samuelokello.dogdom.ui.post.CreatePostScreen
+import com.samuelokello.dogdom.ui.search.SearchScreen
 
 enum class DogdomScreen {
     Login,
     Home,
+    Circle,
+    Release,
     Profile,
+    Message,
     Search,
     Notifications,
     CreatePost
@@ -26,41 +44,104 @@ enum class DogdomScreen {
 @Composable
 fun DogdomScreen(
     homeViewModel: HomeViewModel,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-
     val navController = rememberNavController()
-    val backStackEntry by navController.currentBackStackEntryAsState()
-
+    var showBottomBar by remember { mutableStateOf(false) }
+    val backStackEntry = navController.currentBackStackEntryAsState()
     val currentScreen = DogdomScreen.valueOf(
-        backStackEntry?.destination?.route ?: DogdomScreen.Home.name
+        backStackEntry.value?.destination?.route ?: DogdomScreen.Home.name
     )
     Scaffold { paddingValues ->
-
-        NavHost(
-            navController = navController,
-            startDestination = DogdomScreen.Login.name,
-            modifier = modifier
-        ) {
-            composable(route = DogdomScreen.Login.name) {
-                LoginScreen(
-                    modifier = modifier.padding(paddingValues),
-                    onLoginClick = {
-                        navController.navigate(DogdomScreen.Home.name)
-                    }
-                )
+        BottomNavigationBar( // Place BottomNavigationBar outside NavHost
+            modifier = modifier.padding(paddingValues),
+            showBottomBar = currentScreen in listOf(
+                DogdomScreen.Home,
+                DogdomScreen.Circle,
+                DogdomScreen.Release,
+                DogdomScreen.Message,
+                DogdomScreen.Profile
+            ),
+            onItemClick = { screen ->
+                navController.navigate(screen.name)
             }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = DogdomScreen.Login.name,
+                modifier = modifier
+            ) {
+                composable(route = DogdomScreen.Login.name) {
+                    LoginScreen(
+                        modifier = modifier.padding(innerPadding),
+                        onLoginClick = {
+                            navController.navigate(DogdomScreen.Home.name)
+                        }
+                    )
+                }
+                composable(route = DogdomScreen.Home.name) {
+                    HomeScreen(
+                        viewModel = homeViewModel,
+                        showBottomBar = !showBottomBar,
+                        modifier = modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding),
+                    )
+                }
+                composable(route = DogdomScreen.Circle.name) {
+                    CircleScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+                }
+                composable(route = DogdomScreen.Release.name) {
 
-            composable(route = DogdomScreen.Home.name) {
-                HomeScreen(
-                    viewModel = homeViewModel,
-                    modifier = modifier
-                        .safeDrawingPadding()
-                        .padding(paddingValues),
-                )
+                    ReleaseScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+                }
+                composable(route = DogdomScreen.Profile.name) {
+                    ProfileScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+                }
+                composable(route = DogdomScreen.Message.name) {
+                    MessageScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+                }
+                composable(route = DogdomScreen.Search.name) {
+                    SearchScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+
+                }
+                composable(route = DogdomScreen.Notifications.name) {
+                    NotificationScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+
+                }
+                composable(route = DogdomScreen.CreatePost.name) {
+                    CreatePostScreen(
+                        modifier = Modifier
+                            .safeDrawingPadding()
+                            .padding(innerPadding)
+                    )
+                }
+
             }
-
         }
-
     }
 }
