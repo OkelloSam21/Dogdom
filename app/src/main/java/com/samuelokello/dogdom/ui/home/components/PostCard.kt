6fd11af9068759953fad6cde94bf65a22972e89e
OkelloSam <samuelokello929@gmail.com>
2024-09-14 15:00:32 +0300

    Make PostCard clickable
    This commit makes the `PostCard` composable clickable, allowing users to interact with individual posts.
    
    Clicking on a `Post
    Card` now triggers the `onPostClick` lambda function, passing the post's ID as an argument. This functionality enables navigation to a detailed post view or other actions associated with a specific post.

--- app/src/main/java/com/samuelokello/dogdom/ui/home/components/PostCard.kt
+++ app/src/main/java/com/samuelokello/dogdom/ui/home/components/PostCard.kt
@@ -3,12 +3,28 @@ package com.samuelokello.dogdom.ui.home.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,11 +33,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.samuelokello.dogdom.R
-import com.samuelokello.dogdom.data.DataSource
 import com.samuelokello.dogdom.model.Feature
 import com.samuelokello.dogdom.model.Post
 import com.samuelokello.dogdom.ui.home.HomeAction
@@ -35,8 +49,13 @@ fun PostCard(
     post: Post,
     event: (HomeAction) -> Unit,
     selectTab: Boolean = true,
+    onPostClick: (postId: Int) -> Unit
     ) {
-    Column {
+    Column (
+        modifier = Modifier.clickable (
+            onClick = { onPostClick(post.id) }
+        )
+    ){
         Row(
             modifier
                 .fillMaxWidth()
