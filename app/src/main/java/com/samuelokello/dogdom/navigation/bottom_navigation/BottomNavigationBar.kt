06834a24f1933f92e7fbcf7d585c4724b27dbd68
OkelloSam <samuelokello929@gmail.com>
2024-09-14 14:55:26 +0300

    Correctly handle bottom navigation item clicks
    This commit fixes an issue where clicking on bottom navigation items would not always navigate to the correct screen.
    
    The
     `onItemClick` lambda in `BottomNavigationBar` now uses reflection to find the corresponding `DogdomScreen` object based on the selected item's route. This ensures that the correct screen is displayed when a navigation item is clicked.
    
    This commit also removes the unused `modifier` parameter and the `Preview` compos
    able from `BottomNavigationBar`.

--- app/src/main/java/com/samuelokello/dogdom/navigation/bottom_navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/samuelokello/dogdom/navigation/bottom_navigation/BottomNavigationBar.kt
@@ -2,7 +2,6 @@ package com.samuelokello.dogdom.navigation.bottom_navigation
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
@@ -13,19 +12,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.samuelokello.dogdom.navigation.DogdomScreen
 import com.samuelokello.dogdom.ui.theme.CustomOrange
 
 @Composable
 fun BottomNavigationBar(
-    modifier: Modifier = Modifier,
     showBottomBar: Boolean = false,
     onItemClick: (DogdomScreen) -> Unit,
-    content: @Composable (innerPadding: PaddingValues) -> Unit
+    content: @Composable (innerPadding: PaddingValues) -> Unit = {}
 ) {
     var navigationSelectedItem by remember { mutableIntStateOf(0) }
 
@@ -35,29 +31,32 @@ fun BottomNavigationBar(
                 NavigationBar(
                     containerColor = Color.White
                 ) {
-                    BottomNavigationItem().bottomNavigationItems().forEachIndexed { index, navigationItem ->
-                        NavigationBarItem(
-                            selected = index == navigationSelectedItem,
-                            onClick = {
-                                navigationSelectedItem = index
-                                onItemClick(DogdomScreen.valueOf(navigationItem.route))
-                            },
-                            icon = {
-                                Icon(
-                                    painter = painterResource(id = navigationItem.icon),
-                                    contentDescription = navigationItem.route
+                    BottomNavigationItem().bottomNavigationItems()
+                        .forEachIndexed { index, navigationItem ->
+                            NavigationBarItem(
+                                selected = index == navigationSelectedItem,
+                                onClick = {
+                                    navigationSelectedItem = index
+                                    onItemClick(DogdomScreen::class.sealedSubclasses.mapNotNull { it.objectInstance }
+                                        .find { it.route == navigationItem.route }
+                                        ?: DogdomScreen.Home)
+                                },
+                                icon = {
+                                    Icon(
+                                        painter = painterResource(id = navigationItem.icon),
+                                        contentDescription = navigationItem.route
+                                    )
+                                },
+                                label = {
+                                    Text(text = navigationItem.route)
+                                },
+                                colors = NavigationBarItemDefaults.colors(
+                                    selectedIconColor = Color.White,
+                                    selectedTextColor = CustomOrange,
+                                    indicatorColor = CustomOrange.copy(alpha = 0.8f)
                                 )
-                            },
-                            label = {
-                                Text(text = navigationItem.route)
-                            },
-                            colors = NavigationBarItemDefaults.colors(
-                                selectedIconColor = Color.White,
-                                selectedTextColor = CustomOrange,
-                                indicatorColor = CustomOrange.copy(alpha = 0.8f)
                             )
-                        )
-                    }
+                        }
                 }
             }
         }
