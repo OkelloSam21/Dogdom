f056a78af6a67651c570b818b6e2c4820dbcef9d
OkelloSam <samuelokello929@gmail.com>
2024-09-18 06:56:20 +0300

    Implement CircleItem click action and introduce CircleToJoinComponent
    This commit introduces click functionality to `CircleItemComponent` and adds a new component `Circle
    ToJoinComponent`.
    
    - `CircleItemComponent`: Now accepts an `onClick` lambda parameter, allowing users to interact with individual circle items. The image size is adjusted to 84dp, and the title text color is updated for better readability.
    - `CircleToJoinComponent`: A new component designed to
     display circles available for joining. It features an image, title, member count, and a "Join" button. This component also accepts an `onClick` lambda for handling user interaction.

--- app/src/main/java/com/samuelokello/dogdom/ui/circle/components/CircleToJoinComponent.kt
+++ app/src/main/java/com/samuelokello/dogdom/ui/circle/components/CircleToJoinComponent.kt
@@ -2,9 +2,10 @@ package com.samuelokello.dogdom.ui.circle.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
@@ -15,40 +16,54 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.samuelokello.dogdom.R
 import com.samuelokello.dogdom.ui.circle.CircleItem
+import com.samuelokello.dogdom.ui.shared.components.DogDomButton
 
 @Composable
-fun CircleItemComponent(
+fun CircleToJoinComponent(
     modifier: Modifier = Modifier,
     item: CircleItem,
-    onClick: (id:Int) -> Unit
+    onClick: (id: Int) -> Unit
     ) {
-    Column (
-        modifier.clickable { onClick(item.cId) },
-        horizontalAlignment = Alignment.CenterHorizontally){
+    Row(
+        Modifier.fillMaxWidth()
+            .clickable { onClick(item.cId) },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Absolute.SpaceBetween
+    ) {
         Image(
             painter = painterResource(item.image),
             contentDescription = item.title,
             modifier = modifier
-                .size(84.dp)
+                .size(64.dp)
                 .clip(CircleShape),
             contentScale = ContentScale.Crop
         )
-        Spacer(modifier.height(8.dp))
-        Text(item.title, style = MaterialTheme.typography.labelSmall.copy(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f)))
+
+        Column {
+            Text(item.title, style = MaterialTheme.typography.labelLarge.copy(fontWeight = FontWeight.Bold))
+            Text(
+                "${item.members} members",
+                style = MaterialTheme.typography.labelSmall.copy(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f)
+                )
+            )
+        }
+
+        DogDomButton(
+            label = "Join",
+            onClick = { },
+        )
     }
-
-
 }
-
 
 @Preview(showBackground = true)
 @Composable
-private fun ItemPrev() {
-    CircleItemComponent(
+private fun CircleToJoinPrev() {
+    CircleToJoinComponent(
         item = CircleItem(
             cId = 0,
             title = "Samuel Okello",
